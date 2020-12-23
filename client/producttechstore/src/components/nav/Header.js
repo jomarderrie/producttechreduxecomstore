c4c3f515d8326bf1ddf2b3a0d02b94adda58248f
nathan ganesh <489290@student.saxion.nl>
2020-12-23 14:38:40 +0100

    first commit

---
+++ client/producttechstore/src/components/nav/Header.js
@@ -0,0 +1,31 @@
+import { Menu } from 'antd';
+import React, { useState } from 'react';
+import { AppstoreOutlined, SettingOutlined, UserOutlined, UserAddOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+const { SubMenu, Item } = Menu;
+
+export default function Header() {
+	const [ current, setCurrent ] = useState('');
+
+	const handleClick = (e) => {
+		setCurrent(e.key);
+	};
+
+	return (
+		<Menu onClick={(e) => handleClick(e)} selectedKeys={[ current ]} mode="horizontal">
+			<Item key="home" icon={<AppstoreOutlined />}>
+				<Link to="/">Home</Link>
+			</Item>
+			<SubMenu icon={<SettingOutlined />} title="Username">
+				<Item key="setting:1">Option 1</Item>
+				<Item key="setting:2">Option 2</Item>
+			</SubMenu>
+			<Item key="login" icon={<UserOutlined />} className="float-right">
+				<Link to="/login">Login</Link>
+			</Item>
+			<Item key="register" icon={<UserAddOutlined />} className="float-right">
+				<Link to="/register">Register</Link>
+			</Item>
+		</Menu>
+	);
+}
---
+++ client/producttechstore/src/pages/auth/Register.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { auth } from '../../firebase';
+import { toast } from 'react-toastify';
+
+export default function Register() {
+	const [ email, setEmail ] = useState('');
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const config = {
+			url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
+			handleCodeInApp: true
+		};
+
+		await auth.sendSignInLinkToEmail(email, config);
+		toast.success(`Email is sent to ${email}. Click the link complete your registration.`);
+
+		window.localStorage.setItem('emailForRegistration', email);
+
+		setEmail('');
+	};
+
+	const registerForm = () => {
+		return (
+			<form onSubmit={handleSubmit}>
+				<label>Enter email</label>
+				<input
+					type="email"
+					className="form-control"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					autoFocus
+				/>
+				<button type="submit" className="btn btn-raised">
+					Register now!
+				</button>
+			</form>
+		);
+	};
+
+	return (
+		<div className="container p-5">
+			<div className="row">
+				<div className="col-md-6 offset-md-3">
+					<h4>Register</h4>
+
+					{registerForm()}
+				</div>
+			</div>
+		</div>
+	);
+}
